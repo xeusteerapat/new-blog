9d04a7c3bc326c4b8503ebd2cca3b781662bbf79
xeusteerapat <xeus085@gmail.com>
2021-12-12 22:37:47 +0700

    :lipstick: [Add] add theme and google font

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+
+export default class MyDocument extends NextDocument {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <ColorModeScript />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
