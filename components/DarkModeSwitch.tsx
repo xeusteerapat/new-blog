936c58251882d7cdabccf6c54e662cc4eb74986b
xeusteerapat <xeus085@gmail.com>
2021-12-12 22:43:29 +0700

    :sparkles: [Add] add dark mode switcher

---
+++ components/DarkModeSwitch.tsx
@@ -0,0 +1,21 @@
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import { useColorMode, IconButton } from "@chakra-ui/react";
+
+const DarkModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const iconColor = {
+    light: "black",
+    dark: "white",
+  };
+
+  return (
+    <IconButton
+      aria-label='Toggle dark mode'
+      icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+      onClick={toggleColorMode}
+      color={iconColor[colorMode]}
+    />
+  );
+};
+
+export default DarkModeSwitch;
