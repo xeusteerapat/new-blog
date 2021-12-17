cb458b533bf4cfa158fd1344ade9cc690be9085d
xeusteerapat <xeus085@gmail.com>
2021-12-17 23:32:43 +0700

    :sparkles: [Update] update home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,60 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  useColorMode,
+} from "@chakra-ui/react";
+import customTheme from "../styles/theme";
+import { Global, css } from "@emotion/react";
+import { prismLightTheme, prismDarkTheme } from "../styles/prism";
+
+const GlobalStyle = ({ children }: any) => {
+  const { colorMode } = useColorMode();
+  return (
+    <>
+      <Global
+        styles={css`
+          ${colorMode === "light" ? prismLightTheme : prismDarkTheme};
+          ::selection {
+            background-color: #90cdf4;
+            color: #fefefe;
+          }
+          ::-moz-selection {
+            background: #ffb7b7;
+            color: #fefefe;
+          }
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: ${colorMode === "light" ? "white" : "#171717"};
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={customTheme}>
+      <ColorModeProvider
+        options={{
+          initialColorMode: "light",
+          useSystemColorMode: true,
+        }}
+      >
+        <GlobalStyle>
+          <Component {...pageProps} />
+        </GlobalStyle>
+      </ColorModeProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
